210e74a2dc6d83ed63947eefc448c037868758c0
Brayan Smith Morales <102981505+Brays83@users.noreply.github.com>
2025-05-29 03:11:45 -0500

    chore:commit initial

---
+++ src/main/java/com/pontebarbon/agpontebarbon/AgPonteBarbonApplication.java
@@ -0,0 +1,13 @@
+package com.pontebarbon.agpontebarbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgPonteBarbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgPonteBarbonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pontebarbon/agpontebarbon/AgPonteBarbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontebarbon.agpontebarbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgPonteBarbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
